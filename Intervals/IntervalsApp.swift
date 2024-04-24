8c809c650badd1e6708647058788bcf1eb1da54d
vlmlnkv <vlmlnkv@gmail.com>
2024-04-24 15:59:04 +0700

    Initial Commit

---
+++ Intervals/IntervalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervalsApp.swift
+//  Intervals
+//
+//  Created by Vladimir Melnikov on 24.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct IntervalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
